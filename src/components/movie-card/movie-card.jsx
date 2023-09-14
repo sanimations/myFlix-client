0b4e989116ed85b327836ab5051084b81d348d7d
sanimations <smartucci157@gmail.com>
2023-09-14 15:03:47 -0400

    Updated Favorites to work on a button, only updates with login/out

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,37 +1,69 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Button,Card} from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
+export const MovieCard = ({ user, movie, token }) => {
+  let toggleFav = (user, movie, token) => {
+    let match = user.FavoriteMovies.filter((m) => m == movie.id);
 
-export const MovieCard = ({ user, movie }) => {
-
-
-    return (
-        <Card className="h-100">
-            <Card.Img variant="top" src={movie.image} />
-            <Card.Body>
-                <Card.Title>{movie.title}</Card.Title>
-                <Card.Text>{movie.director.name}</Card.Text>
-                <Link to={ `/movie/` + movie.id}>
-                <Button variant="link">Open</Button>
-                </Link>
-                <Link to={ `/users/` + user.Username + `/movies/` + movie.id}>
-                <Button variant="link">Add/Remove Favorite</Button>
-                </Link>
-            </Card.Body>
-        </Card>
-    );
+    if (match[0] == undefined) {
+      fetch(
+        "https://queer-films-a4556bef0856.herokuapp.com/users/" +
+          user.Username +
+          "/movies/" +
+          movie.id,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(movie),
+        }
+      );
+    } else {
+      fetch(
+        "https://queer-films-a4556bef0856.herokuapp.com/users/" +
+          user.Username +
+          "/movies/" +
+          movie.id,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(movie),
+        }
+      );
+    }
+  };
+  //maybe function here for add/remove instead of link
+  return (
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director.name}</Card.Text>
+        <Link to={`/movie/` + movie.id}>
+          <Button variant="link">Open</Button>
+        </Link>
+        <Button onClick={() => toggleFav(user, movie, token)}>
+          Add/Remove Favorite
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
-
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        image: PropTypes.string.isRequired,
-        director: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-        }).isRequired,
-    }).isRequired
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
