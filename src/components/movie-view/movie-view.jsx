71beb06290e1d8e4d5b9789a35a3780aa98b5bd6
sanimations <smartucci157@gmail.com>
2023-09-28 15:11:01 -0400

    Updated styling in movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -6,41 +6,42 @@ import { useState, useEffect } from "react";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
-    const { movieId } = useParams();
+  const { movieId } = useParams();
 
-    console.log("this is movies: ", movies);
-    console.log("movieId: ", movieId);
-    
-    const movie = movies.find((m) => m.id === movieId);
+  console.log("this is movies: ", movies);
+  console.log("movieId: ", movieId);
 
-    console.log("This is movie: ", movie);
-    return (
+  const movie = movies.find((m) => m.id === movieId);
+
+  console.log("This is movie: ", movie);
+  return (
+    <div className="movie-container">
+      <img src={movie.image} className="movie-poster" alt="Movie Poster" />
+
+      <div className="movie-info">
         <div>
-            <div>
-                <img src={movie.image} />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{movie.description}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director.name}</span>
-                <p>{movie.director.bio}</p>
-                <p>Year of Birth: {movie.director.birth}</p>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.genre.name}</span>
-                <p>{movie.genre.description}</p>
-            </div>
-            <Link to={ `/`}>
-                <button className="back-button">Back</button>
-            </Link>            
+          <span>Title: </span>
+          <span>{movie.title}</span>
         </div>
-    );
+        <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+        </div>
+        <div>
+          <span>Director: </span>
+          <span>{movie.director.name}</span>
+          <p>{movie.director.bio}</p>
+          <p>Year of Birth: {movie.director.birth}</p>
+        </div>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre.name}</span>
+          <p>{movie.genre.description}</p>
+        </div>
+        <Link to={`/`}>
+          <button className="back-button">Back</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
