87100f787faddf0820881ef9c5d42d57ee38390d
sanimations <smartucci157@gmail.com>
2023-09-28 15:11:44 -0400

    Updated styling in signup-view

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,87 +1,91 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import "./signup-view.scss";
 
 export const SignupView = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const [birthday, setBirthday] = useState("");
-    
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        const data = {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birthday: birthday
-          };
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday,
+    };
 
-          console.log("Signup data: ", data);
-      
-          fetch("https://queer-films-a4556bef0856.herokuapp.com/users", {
-          //fetch("http://localhost:8080/users", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-              "Content-Type": "application/json"
-            }
-          }).then((response) => {
-            if (response.ok) {
-              alert("Signup successful");
-              window.location.reload();
-            } else {
-              console.error("Signup failed response: ", response);
-              alert("Signup failed");
-            }
-          });
-        };
+    console.log("Signup data: ", data);
 
-        return(
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                    <Form.Control
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                        minLength="4"
-                    />
-          </Form.Group>
+    fetch("https://queer-films-a4556bef0856.herokuapp.com/users", {
+      //fetch("http://localhost:8080/users", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      if (response.ok) {
+        alert("Signup successful");
+        window.location.reload();
+      } else {
+        console.error("Signup failed response: ", response);
+        alert("Signup failed");
+      }
+    });
+  };
 
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-            </Form.Group>
+  return (
+    <div className="signup-container">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="4"
+          />
+        </Form.Group>
 
-            <Form.Group controlId="formEmail">
-                <Form.Label>Email:</Form.Label>
-                    <Form.Control
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-            </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
 
-            <Form.Group controlId="formBirthday">
-                <Form.Label>Birthday:</Form.Label>
-                    <Form.Control
-                        type="date"
-                        value={birthday}
-                        onChange={(e) => setBirthday(e.target.value)}
-                        required
-                    />
-            </Form.Group>
-                <Button type="submit">Submit</Button>
-            </Form>
-        );
-    }
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button type="submit" className="signup-button">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
