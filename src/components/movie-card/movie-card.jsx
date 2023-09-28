387fb3d24fc787f0a79e0df1f7acadab0e68eb05
sanimations <smartucci157@gmail.com>
2023-09-27 20:08:47 -0400

    updated styling on add/favorite movies in movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,17 +5,19 @@ import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
 export const MovieCard = ({ user, movie, token, setUser }) => {
-  let toggleFav = (user, movie, token) => {
-    let match = user.FavoriteMovies.filter((m) => m == movie.id);
+  const isFavorite = user.FavoriteMovies.includes(movie.id);
+  
+  const toggleFav = () => {
+    const url =
+    "https://queer-films-a4556bef0856.herokuapp.com/users/" +
+      user.Username +
+      "/movies/" +
+      movie.id;
 
-    if (match[0] == undefined) {
-      fetch(
-        "https://queer-films-a4556bef0856.herokuapp.com/users/" +
-          user.Username +
-          "/movies/" +
-          movie.id,
-        {
-          method: "POST",
+    const method = isFavorite ? "DELETE" : "POST";
+    
+      fetch( url, {
+          method: method,
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
@@ -25,46 +27,23 @@ export const MovieCard = ({ user, movie, token, setUser }) => {
       )
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
           localStorage.setItem("user", JSON.stringify(data));
           setUser(JSON.parse(localStorage.getItem("user")));
         });
-    } else {
-      fetch(
-        "https://queer-films-a4556bef0856.herokuapp.com/users/" +
-          user.Username +
-          "/movies/" +
-          movie.id,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify(movie),
-        }
-      )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        localStorage.setItem("user", JSON.stringify(data));
-        setUser(JSON.parse(localStorage.getItem("user")));
-      });
-    }
-  };
+    }; 
   
   return (
     <Card className="h-100">
       <Card.Img variant="top" src={movie.image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.director.name}</Card.Text>
+        <Card.Text>Director: {movie.director.name}</Card.Text>
         <Link to={`/movie/` + movie.id}>
-          <Button variant="primary">Open</Button>
+          <Button variant="primary">Info</Button>
         </Link>
         <span> </span>
-        <Button variant = "dark" onClick={() => toggleFav(user, movie, token)}>
-          Add/Remove Favorite
+        <Button variant = {isFavorite ? "danger" : "dark"} onClick={() => toggleFav(user, movie, token)}>
+          {isFavorite ? "Remove Favorite" : "Add Favorite"}
         </Button>
       </Card.Body>
     </Card>
