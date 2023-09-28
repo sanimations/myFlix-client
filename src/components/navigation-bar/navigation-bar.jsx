39521f42f55bccb22b750ea502f6d096763c5628
sanimations <smartucci157@gmail.com>
2023-09-28 15:11:22 -0400

    Updated styling in navbar

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" fixed="top">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          Movies App
+          Queer Films Database
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
